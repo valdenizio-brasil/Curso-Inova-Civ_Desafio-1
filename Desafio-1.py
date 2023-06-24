55b7a56102f30a6e05dfe6a0f496aa2966a5b151
Valdenizio Brasil <valdenizio.brasil@gmail.com>
2023-06-24 08:36:17 -0300

    Initial_commit

---
+++ Desafio-1.py
@@ -0,0 +1,9 @@
+num1 = int(input('Digite o 1º número: '))
+num2 = int(input('Digite o 2º número: '))
+num3 = int(input('Digite o 3º número: '))
+
+s = num1 + num2 + num3
+
+print(f'Entrada: {num1}, {num2} e {num3}')
+print(f'Processamento {num1} + {num2} + {num3}')
+print(f'Saida: {s}')
